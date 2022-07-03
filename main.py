56f633efd1bdc9dac3e4b71e5210e35763fe77e5
pauloreis7 <paulosilvadosreis2057@gmail.com>
2022-07-03 20:54:18 -0300

    feat: setup pydantic to api

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
-from fastapi import FastAPI
+from typing import Optional, List
+
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {'msg': 'Hello World!'}
+users = []
+
+class User(BaseModel):
+    """Users class attributes"""
+
+    name: str
+    email: str
+    password: str
+    bio: Optional[str]
+
+@app.get('/users', response_model=List[User])
+async def list_users():
+    """Get all users list"""
+
+    return users
+
+@app.get('/users/{user_index}')
+async def find_user(
+    user_index: int = Path(..., description='User index to retrieve', gt=1),
+    query: str = Query(None, max_length=5)
+):
+    """Find user"""
+
+    user = users[user_index]
+
+    return {'user': user, 'query': query}
+
+@app.post('/users')
+async def create_user(user: User):
+    """Create a user"""
+
+    users.append(user)
+
+    return {'msg': True}
